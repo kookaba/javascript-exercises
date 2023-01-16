f071df2e0343d05201c7681f6956075a16518334
Daniel <114618893+kookaba@users.noreply.github.com>
2023-01-16 14:31:55 -0600

    update 07_tempConversion
    - passed first test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return (fahrenheit -32) * 5/9;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return (celsius + 32) * 9/5;
 };
 
 // Do not edit below this line
