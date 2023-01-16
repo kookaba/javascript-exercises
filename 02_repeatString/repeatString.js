8e63d37ba0a030a344e26031a03270ee48cd4182
Daniel <114618893+kookaba@users.noreply.github.com>
2023-01-16 10:52:43 -0600

    completed 02_repeatString
    - added theString, num, sumOfStrings
    - added loop to iterate through num and add theString to sumOfStrings
    - passed test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(theString, num) {
+    let sumOfStrings = '';
+    for (i = 0; i < num; i++) {
+        sumOfStrings += theString;
+    }
+    console.log(sumOfStrings);
 };
 
 // Do not edit below this line
