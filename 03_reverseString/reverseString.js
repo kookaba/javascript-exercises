4288a847fd41ff82e0e973374ce8e1acd5b8fc09
Daniel <114618893+kookaba@users.noreply.github.com>
2023-01-16 11:12:45 -0600

    completed 03_reverseString
    - added theString and reversedString
    - split theString and saved to reversedString
    - reversed reversedString
    - joined reversedString
    - returned reversedString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(theString) {
+    let reversedString = theString.split('');
+    reversedString = reversedString.reverse();
+    reversedString = reversedString.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
