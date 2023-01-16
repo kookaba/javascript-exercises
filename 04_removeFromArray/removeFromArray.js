54223504a8f220f26fbe01cbc9fbbd4422a5f4e3
Daniel <114618893+kookaba@users.noreply.github.com>
2023-01-16 12:21:26 -0600

    completed 04_removeFromArray
    - added myArray and item
    - added index to to get "item" position
    - added splice to remove the element at the index
    - passed first test
    - removed test .skip to complete the rest of the tests
    - removed item and added ...args (which creates an array of an undefined number of arguments)
    - passed second test
    - added if statement to check if a value was present
    - passed the rest of the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...args) {
+    for (var item of args) {
+        if (!myArray.includes(item)) {
+            continue;
+        }
+        else {
+            let index = myArray.indexOf(item);
+            myArray.splice(index, 1);
+        }
 
+        }
+    return(myArray);
 };
 
 // Do not edit below this line
