9c5202cc742317d22dcd974bb851e7fc9211a2fa
Daniel <114618893+kookaba@users.noreply.github.com>
2023-01-18 17:15:43 -0600

    updated 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  var total = 0;
+  for (i=0; i<numbers.length;) {
+    total += numbers[i];
+    i++;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  var total = 1;
+  for (i=0; i<nums.length;) {
+    total *= nums[i];
+    i++;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
+const factorial = function(num) {
 	
 };
 
