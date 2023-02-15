f49d7d0ee5e9c6b5f30f1d99b414cc04a6f9854b
Daniel <114618893+kookaba@users.noreply.github.com>
2023-02-15 16:05:05 -0600

    completed 10_fibonacci
    - passed all tests for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (typeof num === 'string') {
+        num = Math.abs(num);
+    }
+    var x = 0;
+    var y = 1;
+    var next;
+    //create an array
+    var list = [];
+    for (let z = 0; z <= num; z++) {
+        list.push(x);
+        next = x + y;
+        x = y;
+        y = next;
+    }
+    return list[num];
 };
 
 // Do not edit below this line
