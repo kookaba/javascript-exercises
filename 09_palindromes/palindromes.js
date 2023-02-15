729f609a301dcbb8d86c81a50e1c5725ba383e88
Daniel <114618893+kookaba@users.noreply.github.com>
2023-02-15 15:30:38 -0600

    finished 09_palindromes
    - passed all tests for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let backwards = '';
+    word = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    word = word.replaceAll(' ', '');
+    word = word.toLowerCase();
 
+    for (let i = word.length - 1; i >= 0; i--) {
+        backwards += word[i]
+    }
+
+    if (word === backwards) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
